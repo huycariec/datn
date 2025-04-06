54953a9495e90113f4b7efd100bd0fa5d10d0470
typeScript-WEB502 <conglvph41262@gmail.edu.vn>
2025-04-06 19:17:01 +0700

    gd huong dan mh

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function showBuyingGuide()
+     {
+         return view('huongdan');  // hoặc 'pages.huongdan' nếu view trong thư mục 'pages'
+     }
+}
